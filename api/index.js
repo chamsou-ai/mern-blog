917a270f448f8e99b6f3d9ac030404e164038810
chamsou-ai <mekahalc@gmail.com>
2024-03-01 15:57:42 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 const express = require('express')
-
+const mongoose = require("mongoose")
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+  console.log("MongoDB is connected")
+}).catch((err)=>{
+  console.log(err)
+})
 const app = express()
 
 app.listen(3000 , ()=>{
