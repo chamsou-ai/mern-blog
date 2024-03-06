d3df84e4e4fc28e4512ada681df6f882397791e7
chamsou-ai <mekahalc@gmail.com>
2024-03-06 13:20:55 +0100

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 const User = require("../models/user.model.js")
 const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utils/error.js");
+const JWT = require("jsonwebtoken")
 
 const signup = async (req,res , next )=>{
   const {username , email , password} = req.body;
@@ -26,5 +27,33 @@ const signup = async (req,res , next )=>{
     next(error)
   }
 }
+const signin = async (req,res,next)=>{
+  const { email,password } = req.body
+  if(!email || !password || email === "" || password === ""){
+    return next(errorHandler(400,"All fields are required !"))
+  }
+  try {
 
-module.exports = {signup}
+    const validUser = await User.findOne({email})
+    if(!validUser){
+      return next(errorHandler(404,"User not found"))
+    }
+    const validPassword = bcryptjs.compareSync(password,validUser.password)
+    if(!validPassword){
+      return next(errorHandler(404, "Invalid password"))
+    }
+
+    const {password:pass , ...rest} = validUser._doc;
+
+    const token = JWT.sign(
+      {id: validUser._id } , process.env.JWT_SECRET
+    )
+    res.status(200).cookie('access_token',token,{
+      httpOnly:true
+    }).json(rest)
+  } catch (error) {
+    next(error)
+  }
+} 
+
+module.exports = {signup , signin}
